f94461f2c663df685151b8922f818816901d4e70
midouveo-cyber <midouveo@gmail.com>
2026-06-16 00:46:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig( 
+  base: '/Bavarian-Parts/',
+  () => {
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
